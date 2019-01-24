6c7249291ce9db213c1e50832919ae74bbe4f757
Zide Chen <zide.chen@intel.com>
2019-01-24 12:16:05 -0800

    hv: enable MCE from guest CPUID
    Hide the MCE features from IA32_MCG_CAP abd IA32_MCG_STATUS
    
    Tracked-On: #1867
    Signed-off-by: Zide Chen <zide.chen@intel.com>

--- hypervisor/arch/x86/guest/vmsr.c
+++ hypervisor/arch/x86/guest/vmsr.c
@@ -44,6 +44,9 @@ static const uint32_t emulated_guest_msrs[NUM_GUEST_MSRS] = {
 	MSR_IA32_APIC_BASE,
 	MSR_IA32_PERF_CTL,
 	MSR_IA32_FEATURE_CONTROL,
+
+	MSR_IA32_MCG_CAP,
+	MSR_IA32_MCG_STATUS,
 };
 
 #define NUM_MTRR_MSRS	13U
@@ -440,6 +443,12 @@ int32_t rdmsr_vmexit_handler(struct acrn_vcpu *vcpu)
 		v = MSR_IA32_FEATURE_CONTROL_LOCK;
 		break;
 	}
+	case MSR_IA32_MCG_CAP:
+	case MSR_IA32_MCG_STATUS:
+	{
+		v = 0U;
+		break;
+	}
 	default:
 	{
 		if (is_x2apic_msr(msr)) {
@@ -586,6 +595,8 @@ int32_t wrmsr_vmexit_handler(struct acrn_vcpu *vcpu)
 		err = vlapic_wrmsr(vcpu, msr, v);
 		break;
 	}
+	case MSR_IA32_MCG_CAP:
+	case MSR_IA32_MCG_STATUS:
 	case MSR_IA32_FEATURE_CONTROL:
 	{
 		err = -EACCES;
